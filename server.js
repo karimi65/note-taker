2f6c02f374b1547d9f2073b79490a0a4177a0a14
Captain <71038018+karimi65@users.noreply.github.com>
2021-02-02 14:43:37 -0600

    Add HTML and API Routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const http = require('http')
 const fs = require('fs')
 const express = require('express')
 const app = express()
@@ -7,9 +8,24 @@ const PORT = process.env.PORT || 3500
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// HTML ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/public/index.html'))
 })
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+})
+
+// API ROUTES
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + './db/db.json'))
+})
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
